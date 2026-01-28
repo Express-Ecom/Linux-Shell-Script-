94cbf521a2ee1e6acf5ddc459fa981bff7689ff0
Fazarulla Shaik <Fazarulla.Shaik@Bainoona.com>
2026-01-28 21:51:20 +0400

    Action

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # This is a simple Hello World script
 echo "Hello, World!"
+echo "Ramesh :: Hello suresh how are you? "
+echo "Suresh :: I am fine Ramesh, how are you?"
+echo "Ramesh :: I am also fine, thanks for asking."
