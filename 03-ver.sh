22ff849fe3b2561cec7b3c2bc950cc8d092b3cf2
Fazarulla Shaik <Fazarulla.Shaik@Bainoona.com>
2026-01-31 12:40:01 +0400

    Updated files

--- 03-ver.sh
+++ 03-ver.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 Name="Fzarulla"
 echo "Hellow $Name"
-echo "This is version 0.3"
-name="Suresh"
-echo "Welcome, $name! How can I assist you today?"
-#ver to shore the output oa a command 
+#ver to show the host name :
 hostname=$(hostname)
 echo "This script is running on the host: $hostname"
